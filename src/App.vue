cda65b9da675fd1ea11619b0435e8e6d67ffcb4d
atomflunder <80397293+atomflunder@users.noreply.github.com>
2023-05-10 00:53:42 +0200

    Improve style

--- src/App.vue
+++ src/App.vue
@@ -243,76 +243,143 @@ function simulateMore() {
 }
 
 onBeforeMount(() => {
+    stockfish.postMessage(`position fen ${chess.value.fen()}`);
+    setStockfishOptions(stockfish);
+    stockfish.postMessage(`go depth ${stockfishDepth.value}`);
+
     setInterval(simulateMore, 100);
     simulate();
 });
 </script>
 
 <template>
-    <div class="sidebar">
-        <div><button @click="boardAPI?.toggleOrientation()">TOGGLE ORIENTATION</button></div>
-        <div><button @click="boardAPI?.toggleMoves()">TOGGLE MOVES</button></div>
+    <div class="sidebar-left">
+        <div class="boxes">
+            <div class="white-box">
+                <button class="button" @click="gamesStarted = 10">SIMULATE 10 GAMES</button>
 
-        <div>
-            White: {{ Math.floor(whiteElo) }}
-            <br />
-            Black: {{ Math.floor(blackElo) }}
-            <br />
-            Outcomes: W{{ outcomes[0] }} - B{{ outcomes[1] }} - D{{ outcomes[2] }}
+                <img
+                    src="@/assets/knight-white.svg"
+                    alt="pawn-icon"
+                    width="32"
+                    height="32"
+                    class="black-outline"
+                />
+                ELO: {{ Math.floor(whiteElo) }} (W{{ outcomes[0] }} - D{{ outcomes[2] }} - L{{
+                    outcomes[1]
+                }})
+                <select class="select" name="White Algorithm" id="white" v-model="whiteAlgorithm">
+                    <option v-for="(algo, i) in allAlgorithms" :key="i" :value="algo">
+                        {{ algo.name }}
+                    </option>
+                </select>
+            </div>
+
+            <div class="black-box">
+                <button class="button" @click="stopForeground = !stopForeground">
+                    TOGGLE SHOWN GAME
+                </button>
+
+                <img
+                    src="@/assets/knight-black.svg"
+                    alt="pawn-icon"
+                    width="32"
+                    height="32"
+                    class="white-outline"
+                />
+                ELO: {{ Math.floor(blackElo) }} (W{{ outcomes[1] }} - D{{ outcomes[2] }} - L{{
+                    outcomes[0]
+                }})
+                <select class="select" name="Black Algorithm" id="black" v-model="blackAlgorithm">
+                    <option v-for="(algo, i) in allAlgorithms" :key="i" :value="algo">
+                        {{ algo.name }}
+                    </option>
+                </select>
+            </div>
         </div>
 
-        <button @click="gamesStarted = 10">SIMULATE 10 GAMES</button>
-        <button @click="stopForeground = !stopForeground">TOGGLE VISUALISED GAME</button>
+        <div class="stockfish">
+            <img
+                src="@/assets/settings.svg"
+                alt="pawn-icon"
+                width="32"
+                height="32"
+                class="black-outline"
+                style="grid-row: 1"
+            />
 
-        <br />
+            <input
+                class="slider"
+                style="grid-row: 2"
+                type="range"
+                min="0"
+                max="20"
+                v-model="stockfishSkillLevel"
+            />
+            <div style="grid-row: 2">LEVEL: {{ stockfishSkillLevel }}</div>
 
-        WHITE
-        <select name="White Algorithm" id="white" v-model="whiteAlgorithm">
-            <option v-for="(algo, i) in allAlgorithms" :key="i" :value="algo">
-                {{ algo.name }}
-            </option>
-        </select>
+            <input
+                class="slider"
+                style="grid-row: 3"
+                type="range"
+                min="1"
+                max="20"
+                v-model="stockfishDepth"
+            />
+            <div style="grid-row: 3">DEPTH: {{ stockfishDepth }}</div>
 
-        <br />
-        BLACK
-        <select name="Black Algorithm" id="black" v-model="blackAlgorithm">
-            <option v-for="(algo, i) in allAlgorithms" :key="i" :value="algo">
-                {{ algo.name }}
-            </option>
-        </select>
+            <input
+                class="slider"
+                style="grid-row: 4"
+                type="range"
+                min="0"
+                max="5000"
+                v-model="stockfishMaxError"
+            />
+            <div style="grid-row: 4">MAX ERROR: {{ stockfishMaxError }}</div>
 
-        <div>MATERIAL: {{ boardAPI?.getMaterialCount().materialDiff }}</div>
-        <div>EVAL: {{ evaluation }}</div>
-        <div>BEST MOVE: {{ bestMove }}</div>
-        <div>
-            STOCKFISH SETTINGS
-            <br />
-            <input type="range" min="0" max="20" v-model="stockfishSkillLevel" />
-            LEVEL: {{ stockfishSkillLevel }}
-            <br />
-            <input type="range" min="1" max="20" v-model="stockfishDepth" />
-            DEPTH: {{ stockfishDepth }}
-            <br />
-            <input type="range" min="0" max="5000" v-model="stockfishMaxError" />
-            MAX ERROR: {{ stockfishMaxError }}
-            <br />
-            <input type="range" min="1" max="1000" v-model="stockfishProbability" />
-            PROBABILITY: {{ stockfishProbability }}
+            <input
+                class="slider"
+                style="grid-row: 5"
+                type="range"
+                min="1"
+                max="1000"
+                v-model="stockfishProbability"
+            />
+            <div style="grid-row: 5">PROBABILITY: {{ stockfishProbability }}</div>
         </div>
-
-        <div>MOVES:</div>
-        <p style="white-space: pre-line">{{ history }}</p>
     </div>
 
-    <TheChessboard
-        :board-config="boardConfig"
-        @board-created="createBoard"
-        @move="(move) => parseMove(move)"
-    ></TheChessboard>
+    <div class="center">
+        <div>
+            <img src="@/assets/pawn.svg" alt="pawn-icon" width="16" height="16" />
+            {{ boardAPI?.getMaterialCount().materialDiff }}
+            <img src="@/assets/eval.svg" alt="pawn-icon" width="16" height="16" />
+            {{ evaluation }}
+            <img src="@/assets/move.svg" alt="pawn-icon" width="16" height="16" />
+            {{ bestMove }}
+        </div>
+
+        <TheChessboard
+            :board-config="boardConfig"
+            @board-created="createBoard"
+            @move="(move) => parseMove(move)"
+        ></TheChessboard>
+        <button class="button" style="margin-right: 2px" @click="boardAPI?.toggleOrientation()">
+            TOGGLE ORIENTATION
+        </button>
+        <button class="button" style="margin-left: 2px" @click="boardAPI?.toggleMoves()">
+            TOGGLE MOVES
+        </button>
+    </div>
+
+    <div class="sidebar-right">
+        <p style="white-space: pre-line">{{ history }}</p>
+    </div>
 </template>
 
 <style scoped>
-.sidebar {
+.sidebar-left {
     height: 100%;
     width: 400px;
     position: fixed;
@@ -321,5 +388,91 @@ onBeforeMount(() => {
     left: 0;
     overflow-x: hidden;
     padding-top: 20px;
+}
+
+.sidebar-right {
+    height: 100%;
+    width: 400px;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    right: 0;
+    overflow-x: hidden;
+    padding-top: 20px;
+}
+
+@media (max-width: 1700px) {
+    .sidebar-right {
+        display: none;
+    }
+}
+
+.center {
+    justify-content: center;
+    text-align: center;
+    padding: 16px;
+}
+
+.boxes {
+    display: grid;
+    grid-template-columns: 190px 190px;
+    gap: 20px;
+}
+
+.white-box {
+    display: grid;
+}
+
+.black-box {
+    display: grid;
+}
+
+.stockfish {
+    display: grid;
+    margin-top: 100px;
+    grid-template-columns: 245px 150px;
+    gap: 5px;
+}
+
+.button {
+    background-color: #333;
+    border: 2px;
+    color: #fff;
+    padding: 10px;
+    font-size: 16px;
+    cursor: pointer;
+    margin-top: 20px;
+    margin-bottom: 20px;
+}
+
+.button:hover {
+    background-color: #444;
+}
+
+.select {
+    background-color: #333;
+    border: 2px;
+    color: #fff;
+    padding: 10px;
+    font-size: 14px;
+    cursor: pointer;
+}
+
+.select:hover {
+    background-color: #444;
+}
+
+.slider {
+    accent-color: #444;
+}
+
+.white-outline {
+    filter: drop-shadow(-1px -1px 0px #fff) drop-shadow(1px -1px 0px #fff)
+        drop-shadow(1px 1px 0px #fff) drop-shadow(-1px 1px 0px #fff);
+}
+
+.black-outline {
+    filter: drop-shadow(-1px -1px 0px #000) drop-shadow(1px -1px 0px #000)
+        drop-shadow(1px 1px 0px #000) drop-shadow(-1px 1px 0px #000);
 }
 </style>
